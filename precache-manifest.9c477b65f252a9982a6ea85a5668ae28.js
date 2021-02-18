79828150ce045ffb2c35f657110ba2af31fab582
ArcOmotola <ogunsola40@gmail.com>
2021-02-18 11:45:35 +0100

    Updates

---
+++ precache-manifest.9c477b65f252a9982a6ea85a5668ae28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a54bafb58df0e0f06a0405a4ec40391",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "feb900ca2192db171b16",
+    "url": "/react-deploy/static/css/main.31314469.chunk.css"
+  },
+  {
+    "revision": "26c68cf5051dd071fe4b",
+    "url": "/react-deploy/static/js/2.d6c01270.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.d6c01270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb900ca2192db171b16",
+    "url": "/react-deploy/static/js/main.49ac0eb1.chunk.js"
+  },
+  {
+    "revision": "049c17d33acf030fe1a1",
+    "url": "/react-deploy/static/js/runtime-main.d8d8e8d8.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/react-deploy/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
