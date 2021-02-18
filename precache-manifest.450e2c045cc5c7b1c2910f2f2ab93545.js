c210c52dc1de65da3105e167f64757f2b8fd7017
ArcOmotola <ogunsola40@gmail.com>
2021-02-18 13:22:45 +0100

    Updates

---
+++ precache-manifest.450e2c045cc5c7b1c2910f2f2ab93545.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c090514ebfe8903e5beb525cebe579",
+    "url": "/Boxabi/index.html"
+  },
+  {
+    "revision": "198fde44fdbaad70da77",
+    "url": "/Boxabi/static/css/main.92178b3f.chunk.css"
+  },
+  {
+    "revision": "26c68cf5051dd071fe4b",
+    "url": "/Boxabi/static/js/2.d6c01270.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Boxabi/static/js/2.d6c01270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198fde44fdbaad70da77",
+    "url": "/Boxabi/static/js/main.ab4115c4.chunk.js"
+  },
+  {
+    "revision": "324b9640856a248262a9",
+    "url": "/Boxabi/static/js/runtime-main.061eff25.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Boxabi/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-deploy/precache-manifest.9c477b65f252a9982a6ea85a5668ae28.js"
+  "/Boxabi/precache-manifest.450e2c045cc5c7b1c2910f2f2ab93545.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-deploy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Boxabi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
